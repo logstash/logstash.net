93e5785a947c3476fc4c6fdd9e03eb90de7aa58a
Jordan Sissel <jls@semicomplete.com>
2011-05-11 16:16:08 -0700

    - add more routes

--- js/404.js
+++ js/404.js
@@ -1,9 +1,17 @@
 (function() {
   var path = document.location.pathname;
 
+  /* TODO(sissel): Refactor this to be route-like */
+
   // Redirect /issues/NNN to jira.
-  var issues_match = /\/issues\/[^/]+/.exec(path);
+  var issues_match = /^\/issues\/[^/]+/.exec(path);
   if (issues_match) {
     document.location.href = "https://logstash.jira.com/browse/LOGSTASH-" + issues_match[1];
+    return;
+  }
+
+  if (path =~ /^\/issues\/?/) {
+    document.location.href = "https://logstash.jira.com/"
+    return;
   }
 })();
