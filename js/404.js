c482c7f9cf7bdd8fe1d6431a5ddb196aaf37bb62
Jordan Sissel <jls@semicomplete.com>
2011-05-11 16:17:30 -0700

    - fix capturing

--- js/404.js
+++ js/404.js
@@ -4,7 +4,7 @@
   /* TODO(sissel): Refactor this to be route-like */
 
   // Redirect /issues/NNN to jira.
-  var issues_match = /^\/issues\/[^/]+/.exec(path);
+  var issues_match = /^\/issues\/([^/]+)/.exec(path);
   if (issues_match) {
     document.location.href = "https://logstash.jira.com/browse/LOGSTASH-" + issues_match[1];
     return;
