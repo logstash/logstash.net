c8e1845a28e75394290bfdd7da396490ccb2d678
Jordan Sissel <jls@semicomplete.com>
2011-05-11 16:13:45 -0700

    - fix variable name

--- js/404.js
+++ js/404.js
@@ -2,8 +2,8 @@
   var path = document.location.pathname;
 
   // Redirect /issues/NNN to jira.
-  var issues_match = /\/issues\/[^/]+/.exec(path)
+  var issues_match = /\/issues\/[^/]+/.exec(path);
   if (issues_match) {
-    document.location.href = "https://logstash.jira.com/browse/LOGSTASH-" + issues[1];
+    document.location.href = "https://logstash.jira.com/browse/LOGSTASH-" + issues_match[1];
   }
 })();
