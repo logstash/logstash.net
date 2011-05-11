b9964a5fd1f477dbac696a9ceeccbab56c3cabb7
Jordan Sissel <jls@semicomplete.com>
2011-05-11 16:11:47 -0700

    - add 404.js for magic.

---
+++ js/404.js
@@ -0,0 +1,9 @@
+(function() {
+  var path = document.href.pathname;
+
+  // Redirect /issues/NNN to jira.
+  var issues_match = /\/issues\/[^/]+/.exec(path)
+  if (issues_match) {
+    document.location.href = "https://logstash.jira.com/browse/LOGSTASH-" + issues[1];
+  }
+})();
