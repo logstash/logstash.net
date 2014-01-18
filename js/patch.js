31c5d9680563df81902561a845d119bb34ab3a38
Jordan Sissel <jls@semicomplete.com>
2014-01-17 16:14:11 -0800

    - Release 1.3.3

--- js/patch.js
+++ js/patch.js
@@ -1,5 +1,5 @@
 (function() {
-  var latest_version = "1.3.2";
+  var latest_version = "1.3.3";
   var path = document.location.pathname;
   var docs_path_re = new RegExp("/docs/([^/]+)(/.*|$)");
   
